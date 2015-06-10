bd3ae6f643b4d8ef93c0646bdc55529b1f127fbb
rfree2 <rfree@op.pl>
2015-06-10 17:24:27 +0200

    version r/haila (K3) 2015-03 or before to 2015-06

--- motion-start.sh
+++ motion-start.sh
@@ -3,19 +3,16 @@ date=$(date +%Y-%m-%d_%H:%M:%S%z)
 
 echo "guarding-since-$date[$1]" > ~/stat
 
-./lib_sendxmpp.sh "rfree.mobile@jit.si" "starting..."
-sleep 10
-./lib_sendxmpp.sh "rfree.mobile@jit.si" "starting..."
-sleep 60
+bash ./lib_sendxmpp.sh "rfree.mobile@jit.si" "starting soon"
+sleep 3
 
 ./lib_sendxmpp.sh "rfree.mobile@jit.si" "starting...3"
-sleep 5
+sleep 2
 ./lib_sendxmpp.sh "rfree.mobile@jit.si" "starting...2"
-sleep 5
+sleep 2
 ./lib_sendxmpp.sh "rfree.mobile@jit.si" "starting...1"
-sleep 5
-./lib_sendxmpp.sh "rfree.mobile@jit.si" "starting...GO"
-sleep 5
+sleep 2
+bash ./lib_sendxmpp.sh "rfree.mobile@jit.si" "starting...GO"
 motion
-./lib_sendxmpp.sh "rfree.mobile@jit.si" "end of program"
+bash ./lib_sendxmpp.sh "rfree.mobile@jit.si" "end of program"
 
